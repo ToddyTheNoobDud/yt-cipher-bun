e3efd130a3416b0371afca052874840a1165f170
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-10-12 10:43:18 -0300

    Add files via upload

--- worker.ts
+++ worker.ts
@@ -1,11 +1,32 @@
 import { preprocessPlayer } from "./ejs/src/solvers.ts";
+import type { Input, Output } from "./ejs/src/main.ts";
 
-self.onmessage = (e: MessageEvent<string>) => {
+self.onmessage = async (e: MessageEvent<Input>) => {
     try {
-        const output = preprocessPlayer(e.data);
+        const input = e.data;
+        let output: Output;
+
+        if (input.type === 'player') {
+            const preprocessed = preprocessPlayer(input.player);
+            output = {
+                type: 'result',
+                preprocessed_player: input.output_preprocessed ? preprocessed : undefined,
+                responses: []
+            };
+        } else if (input.type === 'preprocessed') {
+            output = {
+                type: 'result',
+                preprocessed_player: input.preprocessed_player,
+                responses: []
+            };
+        } else {
+            throw new Error('Unsupported input type');
+        }
         self.postMessage({ type: 'success', data: output });
+
     } catch (error) {
         const err = error as Error;
+
         self.postMessage({
             type: 'error',
             data: {
