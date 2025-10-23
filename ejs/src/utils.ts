c9607f0f3055bb6008f59d3ed8f18b16bb1c685c
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-10-23 16:24:13 -0300

    Refactor matchesStructure function for clarity

--- ejs/src/utils.ts
+++ ejs/src/utils.ts
@@ -3,50 +3,37 @@ import { type DeepPartial } from "./types.ts";
 
 export function matchesStructure<T extends ESTree.Node>(
   obj: ESTree.Node | ESTree.Node[],
-  structure: DeepPartial<T> | readonly DeepPartial<T>[]
+  structure: DeepPartial<T> | readonly DeepPartial<T>[],
 ): boolean {
   if (Array.isArray(structure)) {
     if (!Array.isArray(obj)) {
       return false;
     }
-
-    const len = structure.length;
-    if (len !== obj.length) {
-      return false;
-    }
-
-    for (let i = 0; i < len; i++) {
-      if (!matchesStructure(obj[i], structure[i])) {
-        return false;
-      }
-    }
-    return true;
+    return (
+      structure.length === obj.length &&
+      structure.every((value, index) => matchesStructure(obj[index], value))
+    );
   }
-
   if (typeof structure === "object") {
     if (!obj) {
       return !structure;
     }
-
     if ("or" in structure) {
-      const orOptions = (structure as { or: unknown[] }).or;
-      for (const option of orOptions) {
-        if (matchesStructure(obj, option)) {
-          return true;
-        }
-      }
-      return false;
+      // Handle `{ or: [a, b] }`
+      return structure.or.some((node) => matchesStructure(obj, node));
     }
-
-    for (const key in structure) {
-      const value = structure[key as keyof typeof structure];
+    if ("anykey" in structure && Array.isArray(structure.anykey)) {
+      // Handle `{ anykey: [a, b] }`
+      const haystack = Array.isArray(obj) ? obj : Object.values(obj);
+      return structure.anykey.every(value => haystack.some((el) => matchesStructure(el, value)));
+    }
+    for (const [key, value] of Object.entries(structure)) {
       if (!matchesStructure(obj[key as keyof typeof obj], value)) {
         return false;
       }
     }
     return true;
   }
-
   return structure === obj;
 }
 
